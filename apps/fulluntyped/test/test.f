8dd2240047259c5a80b346813a441dd223750730
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-26 18:14:03 +0200

    Bring in the test file from OCaml fulluntyped

--- apps/fulluntyped/test/test.f
+++ apps/fulluntyped/test/test.f
@@ -1,12 +1,27 @@
-  /* 1 or 2 examples for testing */
-/* multi
-   line
-   comment */
+/* Examples for testing */
 
-  true  ;
+true;
 if false then true else false; 
 
-/* another comment */
+x/;
+x;
 
-0; succ (pred 0);
+x = true;
+x;
+if x then false else x; 
+
+lambda x. x;
+(lambda x. x) (lambda x. x x); 
+
+{x=lambda x.x, y=(lambda x.x)(lambda x.x)}; 
+{x=lambda x.x, y=(lambda x.x)(lambda x.x)}.x; 
+
+"hello";
+
+timesfloat (timesfloat 2.0 3.0) (timesfloat 4.0 5.0);
+
+0; 
+succ (pred 0);
 iszero (pred (succ (succ 0))); 
+
+let x=true in x;
