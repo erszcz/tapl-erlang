9c19ce0e9ba46a064b6e2986862b4cfe487cd129
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-27 20:13:51 +0200

    Add a test file with less features - test the WIP grammar

---
+++ apps/fulluntyped/test.f
@@ -0,0 +1,16 @@
+true;
+if false then true else false; 
+
+x/;
+x;
+
+x = true;
+x;
+if x then false else x; 
+
+lambda x. x;
+(lambda x. x) (lambda x. x x); 
+
+0; 
+succ (pred 0);
+iszero (pred (succ (succ 0))); 
