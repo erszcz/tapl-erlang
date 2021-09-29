e659c29d7ba0a003e3b1dc7e04eae969d76466a0
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-29 22:45:26 +0200

    Translate main

--- apps/fulluntyped/src/fulluntyped.erl
+++ apps/fulluntyped/src/fulluntyped.erl
@@ -2,6 +2,13 @@
 
 -export([main/1]).
 
+-type command() :: fulluntyped_syntax:command().
+-type context() :: fulluntyped_syntax:context().
+
+-define(lexer,  fulluntyped_lexer).
+-define(parser, fulluntyped_parser).
+-define(syntax, fulluntyped_syntax).
+
 usage(Progname) ->
     io:format("Usage: ~s <source>\n",
               [Progname]).
@@ -11,19 +18,29 @@ main([]) ->
     usage(escript:script_name()),
     erlang:halt(1);
 main([Source]) ->
-    process_file(Source).
+    process_file(Source, ?syntax:empty_context()).
 
-process_file(Source) ->
+process_file(Source, Ctx) ->
     {ok, Data} = file:read_file(Source),
-    {ok, Tokens, _} = fulluntyped_lexer:string(unicode:characters_to_list(Data)),
-    {ok, Commands} = fulluntyped_parser:parse(Tokens),
-    process_commands(Commands).
+    {ok, Tokens, _} = ?lexer:string(unicode:characters_to_list(Data)),
+    {ok, Parser} = ?parser:parse(Tokens),
+    {Commands, _NewCtx} = Parser(Ctx),
+    process_commands(Ctx, Commands).
 
-process_commands([]) -> ok;
-process_commands([C | Commands]) ->
-    case C of
+-spec process_commands(context(), [command()]) -> ok.
+process_commands(Ctx, Commands) ->
+    lists:foldl(fun process_command/2, Ctx, Commands),
+    ok.
+
+-spec process_command(command(), context()) -> context().
+process_command(Command, Ctx) ->
+    case Command of
         {eval, _Info, Term} ->
-            Result = fulluntyped_core:eval(Term),
-            io:format("~ts\n", [fulluntyped_syntax:format_term(Result)])
-    end,
-    process_commands(Commands).
+            Result = fulluntyped_core:eval(Ctx, Term),
+            io:format("~ts\n", [?syntax:format_term(Result)]),
+            Ctx;
+        {bind, _Info, X, Bind} ->
+            Bind_ = fulluntyped_core:eval_binding(Ctx, Bind),
+            io:format("~ts ~ts\n", [X, ?syntax:format_binding(Ctx, Bind_)]),
+            ?syntax:add_binding(Ctx, X, Bind_)
+    end.
