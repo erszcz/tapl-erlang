c7297ebbf592619fc75941f2b57a7f0859c33e63
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-07-05 22:21:23 +0200

    wip: recon: Adjust recon_core.erl

--- apps/recon/src/recon_core.erl
+++ apps/recon/src/recon_core.erl
@@ -3,7 +3,6 @@
 -module(recon_core).
 
 -export([eval/2,
-         eval_binding/2,
          type_of/2,
          types_equiv/3,
          type_error/2, type_error/3]).
@@ -27,16 +26,6 @@ eval(Ctx, T) ->
         T
     end.
 
--spec eval_binding(context(), binding()) -> binding().
-eval_binding(Ctx, B) ->
-    case B of
-        {tm_abb_bind, T, Ty} ->
-            T_ = eval(Ctx, T),
-            {tm_abb_bind, T_, Ty};
-        Bind ->
-            Bind
-    end.
-
 -spec eval1(context(), term_()) -> term_().
 eval1(Ctx, T) ->
     case T of
@@ -50,93 +39,6 @@ eval1(Ctx, T) ->
         {tag, Info, L, T1, Ty} ->
             T1_ = eval1(Ctx, T1),
             {tag, Info, L, T1_, Ty};
-        {case_, Info, T1, Branches} ->
-            case T1 of
-                {tag, _, L, V, _} ->
-                    case is_value(Ctx, V) of
-                        true ->
-                            case lists:keyfind(L, 1, Branches) of
-                                {_, {_, Body}} ->
-                                    ?syntax:term_subst_top(V, Body);
-                                _ ->
-                                    throw(no_rule_applies)
-                            end;
-                        false ->
-                            T1_ = eval1(Ctx, T1),
-                            {case_, Info, T1_, Branches}
-                    end;
-                _ ->
-                    T1_ = eval1(Ctx, T1),
-                    {case_, Info, T1_, Branches}
-            end;
-        {app, Info, T1, T2} ->
-            T1IsVal = is_value(Ctx, T1),
-            T2IsVal = is_value(Ctx, T2),
-            case {T1, T1IsVal, T2IsVal} of
-                {{abs, _, _, _, T12}, _, true} ->
-                    ?syntax:term_subst_top(T2, T12);
-                {_, true, _} ->
-                    T2_ = eval1(Ctx, T2),
-                    {app, Info, T1, T2_};
-                _ ->
-                    T1_ = eval1(Ctx, T1),
-                    {app, Info, T1_, T2}
-            end;
-        {let_, Info, X, T1, T2} ->
-            case is_value(Ctx, T1) of
-                true ->
-                    ?syntax:term_subst_top(T1, T2);
-                false ->
-                    T1_ = eval1(Ctx, T1),
-                    {let_, Info, X, T1_, T2}
-            end;
-        {fix, Info, T1} ->
-            case {T1, is_value(Ctx, T1)} of
-                {{abs, _, _, _, T12}, true} ->
-                    ?syntax:term_subst_top(T, T12);
-                {_, true} ->
-                    throw(no_rule_applies);
-                _ ->
-                    T1_ = eval1(Ctx, T1),
-                    {fix, Info, T1_}
-            end;
-        {var, Info, Index, _} ->
-            case ?syntax:get_binding(Info, Ctx, Index) of
-                {tm_abb_bind, T1, _} ->
-                    T1;
-                _ ->
-                    throw(no_rule_applies)
-            end;
-        {ascribe, Info, T1, Ty1} ->
-            case is_value(Ctx, T1) of
-                true ->
-                    T1;
-                false ->
-                    T1_ = eval1(Ctx, T1),
-                    {ascribe, Info, T1_, Ty1}
-            end;
-        {record, Info, Fields} ->
-            Fields_ = eval_fields(Ctx, Fields),
-            {record, Info, Fields_};
-        {proj, Info, T1, Label} ->
-            case {T1, is_value(Ctx, T1)} of
-                {{record, _, Fields}, true} ->
-                    case lists:keyfind(Label, 1, Fields) of
-                        false -> throw(no_rule_applies);
-                        {_, Val} -> Val
-                    end;
-                {_, false} ->
-                    T1_ = eval1(Ctx, T1),
-                    {proj, Info, T1_, Label}
-            end;
-        {timesfloat, Info, {float, _, F1}, {float, _, F2}} ->
-            {float, Info, F1 * F2};
-        {timesfloat, Info, {float, _, _} = T1, T2} ->
-            T2_ = eval1(Ctx, T2),
-            {timesfloat, Info, T1, T2_};
-        {timesfloat, Info, T1, T2} ->
-            T1_ = eval1(Ctx, T1),
-            {timesfloat, Info, T1_, T2};
         {succ, Info, T1} ->
             T1_ = eval1(Ctx, T1),
             {succ, Info, T1_};
@@ -179,13 +81,7 @@ is_value(Ctx, T) ->
     case T of
         {true, _} -> true;
         {false, _} -> true;
-        {tag, _, _, T1, _} -> is_value(Ctx, T1);
-        {string, _, _} -> true;
-        {float, _, _} -> true;
-        {unit, _} -> true;
         {abs, _, _, _, _} -> true;
-        {record, _, Fields} ->
-            lists:all(fun ({_Label, T1}) -> is_value(Ctx, T1) end, Fields);
         _ ->
             is_numeric_value(Ctx, T)
     end.
@@ -196,105 +92,6 @@ is_numeric_value(Ctx, T) ->
         {zero, _} -> true;
         {succ, _, T1} -> is_numeric_value(Ctx, T1);
         _ -> false
-    end.
-
--spec is_ty_abb(context(), ty() | index()) -> boolean().
-is_ty_abb(Ctx, {var, Index, _}) ->
-    is_ty_abb(Ctx, Index);
-is_ty_abb(Ctx, Index) when is_integer(Index) ->
-    case ?syntax:get_binding(_DummyInfo = {1,1}, Ctx, Index) of
-        {ty_abb_bind, _} -> true;
-        _ -> false
-    end;
-is_ty_abb(_, _) ->
-    false.
-
--spec get_ty_abb(context(), index()) -> ty().
-get_ty_abb(Ctx, Index) ->
-    case ?syntax:get_binding(_DummyInfo = {1,1}, Ctx, Index) of
-        {ty_abb_bind, Ty} -> Ty;
-        _ -> throw(no_rule_applies)
-    end.
-
--spec compute_type(context(), ty()) -> ty().
-compute_type(Ctx, Ty) ->
-    case Ty of
-        {var, Index, _} ->
-            case is_ty_abb(Ctx, Index) of
-                true -> get_ty_abb(Ctx, Index);
-                false -> throw(no_rule_applies)
-            end;
-        {rec, _, TyS1} = TyS ->
-            ?syntax:type_subst_top(TyS, TyS1);
-        _ ->
-            throw(no_rule_applies)
-    end.
-
--spec simplify_type(context(), ty()) -> ty().
-simplify_type(Ctx, Ty) ->
-    try
-        Ty_ = compute_type(Ctx, Ty),
-        simplify_type(Ctx, Ty_)
-    catch throw:no_rule_applies ->
-        Ty
-    end.
-
--spec types_equiv(context(), ty(), ty()) -> boolean().
-types_equiv(Ctx, TyS, TyT) ->
-    types_equiv(Ctx, #{}, TyS, TyT).
-
--spec types_equiv(context(), map(), ty(), ty()) -> boolean().
-types_equiv(Ctx, Seen, TyS, TyT) ->
-    TySIsTyAbb = is_ty_abb(Ctx, TyS),
-    TyTIsTyAbb = is_ty_abb(Ctx, TyT),
-    case maps:get({TyS, TyT}, Seen, false) of
-        true -> true;
-        false ->
-            case {TyS, TyT} of
-                {string, string} -> true;
-                {unit, unit} -> true;
-                {{rec, _, TyS1}, _} ->
-                    types_equiv(Ctx, maps:put({TyS, TyT}, true, Seen),
-                                ?syntax:type_subst_top(TyS, TyS1), TyT);
-                {_, {rec, _, TyT1}} ->
-                    types_equiv(Ctx, maps:put({TyS, TyT}, true, Seen),
-                                TyS, ?syntax:type_subst_top(TyT, TyT1));
-                {{id, IdS}, {id, IdT}} -> IdS == IdT;
-                {float, float} -> true;
-                {{var, IndexS, _}, _} when TySIsTyAbb ->
-                    types_equiv(Ctx, Seen, get_ty_abb(Ctx, IndexS), TyT);
-                {_, {var, IndexT, _}} when TyTIsTyAbb ->
-                    types_equiv(Ctx, Seen, TyS, get_ty_abb(Ctx, IndexT));
-                {{var, Index, _}, {var, Index, _}} ->
-                    true;
-                {{arr, TyS1, TyS2}, {arr, TyT1, TyT2}} ->
-                    types_equiv(Ctx, Seen, TyS1, TyT1)
-                    andalso
-                    types_equiv(Ctx, Seen, TyS2, TyT2);
-                {bool, bool} -> true;
-                {nat, nat} -> true;
-                {{record, Fields1}, {record, Fields2}} ->
-                    length(Fields1) == length(Fields2)
-                    andalso
-                    lists:all(fun ({Label2, Ty2}) ->
-                                      case lists:keyfind(Label2, 1, Fields1) of
-                                          {Label2, Ty1} ->
-                                              types_equiv(Ctx, Seen, Ty1, Ty2);
-                                          false ->
-                                              false
-                                      end
-                              end,
-                              Fields2);
-                {{variant, Fields1}, {variant, Fields2}} ->
-                    length(Fields1) == length(Fields2)
-                    andalso
-                    lists:all(fun ({{L1, Ty1}, {L2, Ty2}}) ->
-                                      L1 == L2 andalso types_equiv(Ctx, Seen, Ty1, Ty2)
-                              end,
-                              lists:zip(Fields1, Fields2));
-                _ ->
-                    false
-            end
     end.
 
 -spec type_of(context(), term_()) -> ty().
@@ -358,25 +155,6 @@ type_of(Ctx, T) ->
                     HeadTy;
                 _ ->
                     type_error(Info, "expected variant type")
-            end;
-        {tag, Info, L, Ti, TyT} ->
-            case simplify_type(Ctx, TyT) of
-                {variant, FieldTypes} ->
-                    case lists:keyfind(L, 1, FieldTypes) of
-                        {L, TyTiExpected} ->
-                            TyTi = type_of(Ctx, Ti),
-                            case types_equiv(Ctx, TyTi, TyTiExpected) of
-                                true ->
-                                    TyT;
-                                false ->
-                                    type_error(Info, "field ~p does not have the expected type ~p",
-                                              [L, TyTiExpected])
-                            end;
-                        false ->
-                            type_error(Info, "label ~p not found", L)
-                    end;
-                _ ->
-                    type_error(Info, "annotation is not a variant type")
             end;
         {var, Info, Index, _} ->
             ?syntax:get_type_from_context(Info, Ctx, Index);
